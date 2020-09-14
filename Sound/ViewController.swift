6774441a9029a2ac3972835a5266f8e3f0f73852
HARUKA ito <haruka_ito@keio.jp>
2020-09-15 01:43:23 +0900

    サウンドアプリ

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func tapDrumButton() {
+        
+      drumSoundPlayer.currentTime = 0
+        
+        drumSoundPlayer.play()
+    
+        
     }
 
 
